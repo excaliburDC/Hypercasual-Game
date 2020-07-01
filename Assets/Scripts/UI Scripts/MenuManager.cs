b5d79825cbcfe855fb14857765a1a85ec7303d01
DC <43692686+excaliburDC@users.noreply.github.com>
2020-07-01 12:36:19 +0530

    Created Menu System (Subject to Change)

---
+++ Assets/Scripts/UI Scripts/MenuManager.cs
@@ -0,0 +1,98 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuManager : MonoBehaviour
+{
+    public Menus m_startMenu;
+
+    //for testing purpose
+    [SerializeField] public Menus m_pauseMenu;
+
+    [SerializeField]
+    private Component[] menus = new Component[0];
+
+
+    public delegate void SwitchDelegate();
+    public event SwitchDelegate onSwitchedMenu;
+
+    private Menus previousMenu;
+    public Menus PreviousMenu { get { return previousMenu; } }
+
+    private Menus currentMenu;
+    public Menus CurrentMenu { get { return currentMenu; } }
+
+    private void Awake()
+    {
+        //the bool parameter decides whether to include inactive gameobjects or not
+        menus = GetComponentsInChildren<Menus>(true);
+    }
+
+    private void Start()
+    {
+        if (m_startMenu)
+        {
+            SwitchMenus(m_startMenu);
+        }
+    }
+
+    private void Update()
+    {
+        //for testing purpose only
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            SwitchMenus(m_pauseMenu);
+        }
+    }
+
+    public void SwitchMenus(Menus menu)
+    {
+        if (menu)
+        {
+            if (currentMenu)
+            {
+                currentMenu.CloseMenu();
+                previousMenu = currentMenu;
+            }
+            currentMenu = menu;
+           // currentMenu.gameObject.SetActive(true);
+            currentMenu.ActivateMenu();
+
+            if (onSwitchedMenu != null)
+            {
+                Debug.Log("Event Fired");
+                // onSwitchedMenu += new SwitchDelegate();
+            }
+        }
+    }
+
+    public void SwitchToPreviousMenu()
+    {
+        if (previousMenu)
+        {
+            SwitchMenus(previousMenu);
+        }
+    }
+
+    public void QuitGame()
+    {
+        #if UNITY_EDITOR
+            // Application.Quit() does not work in the editor so
+            // UnityEditor.EditorApplication.isPlaying need to be set to false to end the game
+            UnityEditor.EditorApplication.isPlaying = false;
+        #else
+            Application.Quit();
+        #endif
+    }
+
+    public void LoadScene(int sceneIndex)
+    {
+        StartCoroutine(WaitToLoadScene(sceneIndex));
+
+    }
+
+    private IEnumerator WaitToLoadScene(int index)
+    {
+        yield return null;
+    }
+}
---
+++ Assets/Scripts/UI Scripts/Menus.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class Menus : MonoBehaviour
+{
+    public Selectable m_startSelectable;
+
+    public delegate void ActivateMenuDelegate();
+    public delegate void CloseMenuDelegate();
+
+    public event ActivateMenuDelegate onActivateMenu;
+    public event CloseMenuDelegate onCloseMenu;
+
+   // private Animator anim;
+
+    private void Awake()
+    {
+       // anim = GetComponent<Animator>();
+    }
+
+    private void Start()
+    {
+        if (m_startSelectable)
+        {
+            EventSystem.current.SetSelectedGameObject(m_startSelectable.gameObject);
+        }
+    }
+
+    public virtual void ActivateMenu()
+    {
+        if (onActivateMenu != null)
+        {
+            //fire activate menu event
+        }
+        gameObject.SetActive(true);
+       // HandleAnimator("Show");
+    }
+
+    public virtual void CloseMenu()
+    {
+        if (onCloseMenu != null)
+        {
+            //fire close menu event
+        }
+        gameObject.SetActive(false);
+       // HandleAnimator("Hide");
+    }
+
+    //private void HandleAnimator(string animTrigger)
+    //{
+    //    if (anim)
+    //    {
+    //        anim.SetTrigger(animTrigger);
+    //    }
+    //}
+
+}
